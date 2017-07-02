9255643e8b32af00c808afc09b0a3ac7aac74d43
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2017-07-02 19:49:10 +0530

    Removed reading of topview files from 'N' and 'n' commands

--- src/controlUI/ImageView.cpp
+++ src/controlUI/ImageView.cpp
@@ -695,33 +695,12 @@ ImageView::on_key_down(int key)
 	// Key n - Uses the points written from a file (obtained from key c)
 	else if(key == 'n')
 	{
-		/*struct passwd *pw = getpwuid(getuid());
-		const char *homedir = pw->pw_dir;
-		string str_home_dir(homedir); 
-		string inputDirectory = str_home_dir + "/";
-		string filename = inputDirectory + "Plane_Info.txt";*/
 		string plane_filename = "Plane_Info.txt";
-		string topview_filename = "TopViewInfo.txt";
 		vector< vector<float> > sortedPlaneParameters;
 		vector< vector<Point3f> > boundingBoxPoints;
 		readPlaneInfo(plane_filename, sortedPlaneParameters, boundingBoxPoints);
 		PRINT_LOG(1, print2dVector(sortedPlaneParameters, "Plane Parameters:\n", ""));
 		PRINT_LOG(1, print2dVector(boundingBoxPoints, "Bounding Box points:\n", ""));
-		/* Launches GUI: Return approx. angles and orientations */
-		/* Angle with which	quadcopter has to rotate to orient itself to the new plane */
-		std::vector< double > main_angles;
-		/* Direction in which quadcopter should rotate to orient its yaw with normal of new plane */
-		std::vector< int > directions;
-		std::vector< RotateDirection > main_directions;
-		/* Printing the information to the terminal */
-		int number_of_planes = 0;
-		int type_of_surface = 0;
-		float max_height_of_plane = 0.0f;
-		int view_dir = 0;
-		readTopViewInfo(topview_filename, number_of_planes, type_of_surface,
-						max_height_of_plane, main_angles,
-						directions);
-		PRINT_LOG(1, print1dVector(directions, "Directions of rotation:\n", ""));
 		if(renderRect)
 		{
 			renderRect = false;  // While moving the quadcopter we don't want bounding box to appear
@@ -749,27 +728,9 @@ ImageView::on_key_down(int key)
 	else if(key == 'N')
 	{
 		string plane_filename = "Plane_Info.txt";
-		string topview_filename = "TopViewInfo.txt";
 		vector< vector<float> > sortedPlaneParameters;
 		vector< vector<Point3f> > boundingBoxPoints;
 		readPlaneInfo(plane_filename, sortedPlaneParameters, boundingBoxPoints);
-		PRINT_LOG(1, print2dVector(sortedPlaneParameters, "Plane Parameters:\n", ""));
-		PRINT_LOG(1, print2dVector(boundingBoxPoints, "Bounding Box points:\n", ""));
-		/* Launches GUI: Return approx. angles and orientations */
-		/* Angle with which	quadcopter has to rotate to orient itself to the new plane */
-		std::vector< double > main_angles;
-		/* Direction in which quadcopter should rotate to orient its yaw with normal of new plane */
-		std::vector< int > directions;
-		std::vector< RotateDirection > main_directions;
-		/* Printing the information to the terminal */
-		int number_of_planes = 0;
-		int type_of_surface = 0;
-		float max_height_of_plane = 0.0f;
-		int view_dir = 0;
-		readTopViewInfo(topview_filename, number_of_planes, type_of_surface,
-						max_height_of_plane, main_angles,
-						directions);
-		PRINT_LOG(1, print1dVector(directions, "Directions of rotation:\n", ""));
 		if(renderRect)
 		{
 			renderRect = false;  // While moving the quadcopter we don't want bounding box to appear
@@ -798,8 +759,11 @@ ImageView::on_key_down(int key)
 			boundingBoxPoints[i][4] = one;
 		}
 		string filename = "Reversed_Plane_Info.txt";
-        PRINT_LOG(1, "Writing info gathered to " << filename << "\n");
-        for (unsigned int i = 0; i < sortedPlaneParameters.size(); ++i)
+        PRINT_LOG(0, "Writing info gathered to " << filename << "\n");
+        PRINT_LOG(0, "Total number of planes: " << sortedPlaneParameters.size() << ".\n");
+        PRINT_LOG(0, print2dVector(sortedPlaneParameters, "Plane Parameters:\n", ""));
+		PRINT_LOG(0, print2dVector(boundingBoxPoints, "Bounding Box points:\n", ""));
+		for (unsigned int i = 0; i < sortedPlaneParameters.size(); ++i)
         {
             WriteInfoToFile(boundingBoxPoints[i], sortedPlaneParameters[i], i+1, filename);
         }
